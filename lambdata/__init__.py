9dc8eb8bdc4a02351dbc6f8db94c8549429c82e5
kevmanning <kevmanning@protonmail.com>
2021-06-22 22:57:29 -0400

    first module of Sprint 1 for dspt11

---
+++ lambdata/__init__.py
@@ -0,0 +1,11 @@
+"""collection of data science helper functions"""
+
+import pandas
+import numpy
+
+def df_cleanser(df):
+    """cleans dataframe"""
+    # TODO - implement
+
+    pass
+
---
+++ lambdata/example_module.py
@@ -0,0 +1,5 @@
+"""a module in lambdata"""
+
+favorite_animals = ['tree frog', 'white rhino', 'zebra']
+
+favorite_numbers = ['thirty-four', 'eight']
